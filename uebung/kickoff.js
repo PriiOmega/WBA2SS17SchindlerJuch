5a726beb672e1f2db67f27a2e47952905ef0ff9a
bjuch <benjamin.juch@gmail.com>
2017-05-04 15:49:02 +0200

    Update kickoff.js

--- uebung/kickoff.js
+++ uebung/kickoff.js
@@ -6,11 +6,30 @@ fs.readFile('staedte.json',function(err,data){
 
 
 // String in JS-Objekt umwandeln
+   
     
-    var obj = JSON.parse(data.toString())
+ var obj = JSON.parse(data);
+    
+ 
     
 // Daten aus Staedte entnehmen
     
-    var Stadt =obj.staedte;
+ //  var Stadt =obj.staedte;
     
+      console.log(obj)    ; 
+
+
+});
+
+
+// Asynchrones Schreiben
     
+
+//  fs.writeFile('test1.txt', 'kickoff.js',function(err){
+                 
+                 
+         
+                 
+                 
+                 
+                // });
